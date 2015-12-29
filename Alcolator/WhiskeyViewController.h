56c29becf44aba55646c43057a2f65463e99e81d
randallkanna <randallkanna@gmail.com>
2015-12-28 17:39:32 -0800

    implementing calculations

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Randall Kanna on 12/28/15.
+//  Copyright (c) 2015 Randall Kanna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
